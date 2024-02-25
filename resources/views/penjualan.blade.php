900c872aa24319466587e55a113c717f4c4d45c7
FaridFitriansahAlfarizi <faridgitriansahalfarizi@gmail.com>
2024-02-25 16:52:36 +0700

    first commit

---
+++ resources/views/penjualan.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Penjualan | POS</title>
+</head>
+<body>
+    <h1>Halaman Penjualan Point Of Sales</h1>
+    <h3>Berisi semua transaksi POS</h3>
+</body>
+</html>
