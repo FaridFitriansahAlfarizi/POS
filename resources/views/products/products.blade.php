b0634e1fbd14172b30db10ac969d08a5b93c08de
FaridFitriansahAlfarizi <faridgitriansahalfarizi@gmail.com>
2024-02-25 17:21:16 +0700

    Mengubah isi README & Menambahkan Link List Pada Halaman Products

--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -8,5 +8,11 @@
 </head>
 <body>
     <h1>Halaman Products Point Of Sales</h1>
+    <ul>
+        <li><a href="category/food-beverage">Food Beverage</a></li>
+        <li><a href="category/beauty-health">Beauty Health</a></li>
+        <li><a href="category/home-care">Home Care</a></li>
+        <li><a href="category/baby-kid">Baby Kid</a></li>
+    </ul>
 </body>
 </html>
